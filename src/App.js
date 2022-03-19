d79ae74d3f677305dee6d5f150d162f7ad7814c1
abetomoki0420 <kimi.jack.of.all.trades@gmail.com>
2022-03-19 16:38:29 +0900

    feat: styling with daisyUI CDN

--- src/App.js
+++ src/App.js
@@ -35,22 +35,38 @@ function App({ client }) {
   if (error) return "An error has occurred"
 
   return (
-    <>
-      <h1>Users</h1>
-      <ul>
-        {data && data.map((user) => <li key={user.name}>{user.name}</li>)}
-      </ul>
-      <div>
+    <div className="m-4">
+      <h1 className="text-3xl">Users</h1>
+      <table className="table table-compact mt-2">
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.map((user, index) => (
+              <tr key={index}>
+                <th>{index + 1} </th>
+                <th>{user.name}</th>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      <div className="mt-2">
         {mutation.isLoading ? (
           <div>Processing...</div>
         ) : (
           <div>
             <input
+              className="input input-sm input-bordered bg-white"
               value={userName}
               type="input"
               onChange={(e) => setUserName(e.target.value)}
             />
             <button
+              className="btn ml-2 btn-primary btn-sm"
               onClick={() =>
                 mutation.mutate({
                   name: userName,
@@ -62,7 +78,7 @@ function App({ client }) {
           </div>
         )}
       </div>
-    </>
+    </div>
   )
 }
 
